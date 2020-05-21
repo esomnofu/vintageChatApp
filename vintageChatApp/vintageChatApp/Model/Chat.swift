32d959d9132a1f265c7f19ab2dcdaf6933088903
Christian Esomnofu <mresomnofu@gmail.com>
2020-05-21 11:33:22 +0100

    refactor: Addition of Folder Grouping, creation of Chat Model

---
+++ vintageChatApp/vintageChatApp/Model/Chat.swift
@@ -0,0 +1,16 @@
+//
+//  Chat.swift
+//  vintageChatApp
+//
+//  Created by Malik on 21/05/2020.
+//  Copyright © 2020 MrOfu. All rights reserved.
+//
+
+import Foundation
+
+struct Chat {
+    let message : String
+    let isFromReceiver : Bool
+    let name : String
+    let time : String
+}
